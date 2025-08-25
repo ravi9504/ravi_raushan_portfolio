73943c0f26612a40123260184cb20257f63991f5
Ravi Raushan <raviraushan85074@gmail.com>
2025-08-25 13:34:35 +0530

    ProjectCard.jsx

--- src/sections/Skills/Skills.jsx
+++ src/sections/Skills/Skills.jsx
@@ -1,8 +1,16 @@
-import styles from './Skills.modules.css'
+import styles from "./Skills.modules.css";
+import checkMarkIcon from '../../assets/checkmark-light.svg'
 function Skills() {
-  return (
-   <section id='skills' className={styles.container}></section>
+  return( 
+    <section id="skills" className={styles.container}>
+        <h1>SKills</h1>
+        <div className={styles.skillList}>
+            <span>
+                <img src={checkMarkIcon} alt="" />
+            </span>
+        </div>
+    </section>
   )
 }
 
-export default Skills
+export default Skills;
